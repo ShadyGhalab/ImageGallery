a7a73458e95b39eb90d710737065732124868225
Shady Ghalab <shadyghalab@gmail.com>
2019-10-12 14:48:31 +0200

    Make the imageView rounded

--- ImageGallery/Controllers/Cells/ImageGalleryCollectionViewCell.swift
+++ ImageGallery/Controllers/Cells/ImageGalleryCollectionViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 import ReactiveSwift
 import ReactiveCocoa
 
+private enum Constants {
+    static let borderWidth: CGFloat = 0.5
+    static let cornerRadius: CGFloat = 5
+}
+
 final class ImageGalleryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
 
@@ -17,6 +22,7 @@ final class ImageGalleryCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        makeImageViewRounded()
         bindViewModel()
     }
     
@@ -26,6 +32,14 @@ final class ImageGalleryCollectionViewCell: UICollectionViewCell {
         .observeValues({ [unowned self] image in
             self.imageView.image = image
         })
+    }
+    
+    private func makeImageViewRounded() {
+        imageView.layer.borderWidth = Constants.borderWidth
+        imageView.layer.masksToBounds = false
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.layer.cornerRadius = Constants.cornerRadius
+        imageView.clipsToBounds = true
     }
     
     override func prepareForReuse() {
